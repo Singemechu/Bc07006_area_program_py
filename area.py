de2a1a0a00c77d6da3f2a866c713ec3ebc1a95b1
John Smith <sinidu.gemechu@students.vu.edu.au>
2022-10-12 21:51:14 +1100

    Assesment 1a.area of rectangle programing

---
+++ area.py
@@ -0,0 +1,13 @@
+#program area of rectangle
+#Formula for area of rectangle is height *width(maths)
+#assign value for height and width by using float() and input()
+#float allow value with decimal ass it is
+#input to enter  value of height and width and automatically calculate areaby using formula
+#Define formula
+#print the string and value of area
+
+height = float(input('Enter the height of a Rectangle: '))
+width= float(input('Enter the width of a Rectangle: '))
+Area = height*width
+print("Area of a Rectangle is: %.2f" %Area)
+
